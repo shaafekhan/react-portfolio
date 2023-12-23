9de0e00b3c529cc565002a062e603a52dd8c76ae
shaafekhan <56558384+shaafekhan@users.noreply.github.com>
2023-12-23 02:10:17 +0100

    add custom google font and set background color

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,24 +13,24 @@ const Sidenav = () => {
         <div>
             <AiOutlineMenu onClick={handleNav} color={'white'} className='absolute top-4 right-4 /*set to left */ z-[99] md:hidden'/>
             { nav ? (
-                <div className='fixed w-full h-96 bg-black/90 flex flex-col justify-center items-center z-20'>
-                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md bg-neutral-600 shadow-neutral-900 m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-20'>
+                <div className='fixed w-full h-96 bg-stone-950 flex flex-col justify-center items-center z-20'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md outline outline-2 outline-stone-600 bg-stone-950 shadow-neutral-900 m-2 p-3 cursor-pointer hover:bg-stone-600 ease-in duration-150'>
                         <AiOutlineHome size={20} color={'white'} />
                         <span className='pl-4 text-white'>Home</span>
                     </a>
-                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md bg-neutral-600 shadow-neutral-900 m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-20'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md outline outline-2 outline-stone-600 bg-stone-950 shadow-neutral-900 m-2 p-3 cursor-pointer hover:bg-stone-600 ease-in duration-150'>
                         <GrProjects size={20} color={'white'} />
                         <span className='pl-4 text-white'>Work</span>
                     </a>
-                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md bg-neutral-600 shadow-neutral-900 m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-20'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md outline outline-2 outline-stone-600 bg-stone-950 shadow-neutral-900 m-2 p-3 cursor-pointer hover:bg-stone-600 ease-in duration-150'>
                         <AiOutlineProject size={20} color={'white'} />
                         <span className='pl-4 text-white'>Projects</span>
                     </a>
-                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md bg-neutral-600 shadow-neutral-900 m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-20'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md outline outline-2 outline-stone-600 bg-stone-950 shadow-neutral-900 m-2 p-3 cursor-pointer hover:bg-stone-600 ease-in duration-150'>
                         <BsPerson size={20} color={'white'} />
                         <span className='pl-4 text-white'>Resume</span>
                     </a>
-                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md bg-neutral-600 shadow-neutral-900 m-2 p-3 cursor-pointer hover:scale-105 ease-in duration-20'>
+                    <a href='#main' className='w-[75%] flex justify-center items-center rounded-md shadow-md outline outline-2 outline-stone-600 bg-stone-950 shadow-neutral-900 m-2 p-3 cursor-pointer hover:bg-stone-600 ease-in duration-150'>
                         <AiOutlineMail size={20} color={'white'} />
                         <span className='pl-4 text-white'>Contact</span>
                     </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Cabin', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
